d7b398ba5b0e75962aa0085612a515862410d296
mantou <676015863@qq.com>
2019-09-17 17:47:44 +0800

    插件挂载的方法增加

--- README.md
+++ README.md
@@ -11,3 +11,70 @@ import { mountPlugin } from 'h5ds-mount-plugin';
 mountPlugin({...});
 
 ```
+# API
+
+#### mountPlugin `v1.0.0 add`
+
+说明：挂载单个插件
+
+```javascript
+
+import { mountPlugin } from 'h5ds-mount-plugin';
+
+// 挂载demo插件
+mountPlugin({...demo});
+
+```
+
+#### unmountPlugin `v1.0.0 add`
+
+
+说明：卸载挂载单个插件
+
+```javascript
+
+import { unmountPlugin } from 'h5ds-mount-plugin';
+
+// 卸载demo插件
+unmountPlugin('demo');
+
+```
+
+#### mountValue `v1.1.0 add`
+
+说明：挂载指定参数
+
+```javascript
+
+import { mountValue } from 'h5ds-mount-plugin';
+
+// 挂载 swiper 名称的参数，value
+mountValue('swiper', value);
+
+```
+
+#### unmountValue `v1.1.0 add`
+
+说明：卸载指定参数
+
+```javascript
+
+import { unmountValue } from 'h5ds-mount-plugin';
+
+// 卸载 swiper 名称的值
+unmountValue('swiper');
+
+```
+
+#### unmountAll `v1.1.0 add`
+
+说明：卸载所有参数
+
+```javascript
+
+import { unmountAll } from 'h5ds-mount-plugin';
+
+// 卸载所有参数
+unmountAll('');
+
+```
--- src/index.js
+++ src/index.js
@@ -5,15 +5,54 @@ function mountPlugin(data) {
       plugins: {}
     };
   }
-  data = { ...data };
-  window.H5DS_GLOBAL.plugins[data.config.pid] = data;
+  try {
+    window.H5DS_GLOBAL.plugins[data.config.pid] = { ...data };
+  } catch (e) {
+    console.error('插件资源格式错误！', data, e);
+  }
 }
 
 // 卸载插件
 function unmountPlugin(pluginName) {
   if (window.H5DS_GLOBAL) {
-    delete window.H5DS_GLOBAL.plugins[pluginName];
+    try {
+      document.getElementById(`css_${pluginName}`).remove();
+      document.getElementById(`js_${pluginName}`).remove();
+      delete window.H5DS_GLOBAL.plugins[pluginName];
+    } catch (e) {
+      console.error('卸载失败', e);
+    }
+  } else {
+    console.warn('未挂载任何插件');
   }
 }
 
-export { mountPlugin, unmountPlugin };
+// 挂载其他参数
+function mountValue(key, value) {
+  if (!window.H5DS_GLOBAL) {
+    window.H5DS_GLOBAL = {
+      plugins: {}
+    };
+  }
+  window.H5DS_GLOBAL[key] = value;
+}
+
+// 卸载其他插件
+function unmountValue(key) {
+  if (window.H5DS_GLOBAL) {
+    if (window.H5DS_GLOBAL[key]) {
+      delete window.H5DS_GLOBAL[key];
+    } else {
+      console.error('未找到对应的挂载参数', key);
+    }
+  } else {
+    console.warn('未挂载任何插件');
+  }
+}
+
+// 卸载所有参数
+function unmountAll() {
+  window.H5DS_GLOBAL = {};
+}
+
+export { mountPlugin, unmountPlugin, mountValue, unmountValue, unmountAll };
